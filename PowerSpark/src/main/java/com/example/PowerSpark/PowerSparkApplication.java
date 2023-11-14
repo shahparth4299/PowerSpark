0aa3db3c166883e1d7c942aa25e8ed787e8550b0
ADMIN <parth4299.shah@gmail.com>
2023-11-14 23:07:27 +0000

    first commit

---
+++ PowerSpark/src/main/java/com/example/PowerSpark/PowerSparkApplication.java
@@ -0,0 +1,13 @@
+package com.example.PowerSpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerSparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerSparkApplication.class, args);
+	}
+
+}
---
+++ PowerSpark/src/test/java/com/example/PowerSpark/PowerSparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PowerSpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerSparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
